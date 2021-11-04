bf2c086b8d0e78642c4208cb582dac882f24f116
Arslan Semenov <59603766+em1tao@users.noreply.github.com>
2021-11-04 19:28:31 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 USERDATA = config['LOGIN']
-USER = USERDATA['User']
-PASSWORD = USERDATA['Password']
+USER = USERDATA['username']
+PASSWORD = USERDATA['password']
