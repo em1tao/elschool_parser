f1acf77ef98291b5f5ac3df6899b8d2f36619935
Arslan Semenov <59603766+em1tao@users.noreply.github.com>
2021-11-04 19:36:43 +0500

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ USERNAME = config.USERNAME
 PASSWORD = config.PASSWORD
 a = Parser(USERNAME, PASSWORD)
 a.parse()
+
 for i in range(6):
     for k in a[i]:
-        print(f"{k.name}: {k.homework }")
+        print(f"{k.name}: {k.homework}")
