eff2ed4edc5ddb7f3f7ea6ca127ca7389a1e8a7d
Arslan Semenov <59603766+em1tao@users.noreply.github.com>
2021-11-04 19:29:08 +0500

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from parser import Parser
 
 USERNAME = config.USERNAME
 PASSWORD = config.PASSWORD
-a = Parser(USER, PASSWORD)
+a = Parser(USERNAME, PASSWORD)
 a.parse()
 for i in range(6):
     for k in a[i]:
